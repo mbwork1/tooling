8a82d4096886a3ebb5ba52be0317d72a9ce6e26d
mbwork1 <mbwork1@yahoo.com>
2024-11-03 18:44:30 -0500

    Update backend.tf1

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "tooling/terraform.state"
-    bucket         = "dominion-terraform-backend-bucket"
+    bucket         = "west-eks-tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform-state-locking"
+    dynamodb_table = "terraform-state-lock-table"
   }
 }
